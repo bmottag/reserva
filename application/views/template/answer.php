34cc4467c49bbc78a3e803745fe3e552e886c3ac
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2018-03-30 09:42:28 -0600

    Se carga vista para mostrar resutados

--- application/views/template/answer.php
+++ application/views/template/answer.php
@@ -1,54 +1,31 @@
-<div id="page-wrapper">
-	<br>
+<div class="right_col" role="main">
 	<div class="row">
-		<div class="col-md-12">
-			<div class="panel panel-primary">
-				<div class="panel-heading">
-					<h4 class="list-group-item-heading">
-						<?php echo $titulo; ?>
-					</h4>
+		<div class="col-md-12 col-sm-12 col-xs-12">
+			<div class="x_panel">
+				<div class="x_title">
+					<h2><?php echo $titulo; ?></h2>
+					<ul class="nav navbar-right panel_toolbox">
+						<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+						</li>
+					</ul>
+					<div class="clearfix"></div>
+				</div>
+				<div class="x_content">
+				
+					<div class="row" align="center">
+						<div style="width:50%;" align="center">
+							<div class="alert <?php echo $clase;?> fade in" role="alert">
+								<?php echo $msj; ?>
+							</div>
+							
+							<?php if($linkBack){ ?>
+							<a class="btn btn-success" href=" <?php echo base_url($linkBack); ?> "><span class="glyphicon glyphicon glyphicon-chevron-left" aria-hidden="true"></span> Regresar </a> 
+							<?php } ?>
+						</div>
+					</div>	
+					
 				</div>
 			</div>
 		</div>
-		<!-- /.col-lg-12 -->				
 	</div>
-	
-	<!-- /.row -->
-	<div class="row">
-		<div class="col-lg-12">
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<?php if($linkBack){ ?>
-					<a class="btn btn-success" href=" <?php echo base_url($linkBack); ?> "><span class="glyphicon glyphicon glyphicon-chevron-left" aria-hidden="true"></span> Regresar </a> 
-					<?php } ?>
-					<?php echo $titulo; ?>
-				</div>
-				<div class="panel-body">
-					<div class="row">
-						<div class="col-lg-12">
-						
-							<div class="row" align="center">
-								<div style="width:50%;" align="center">
-									<div class="alert <?php echo $clase;?>"> <span class="glyphicon glyphicon-ok">&nbsp;</span>
-										<?php echo $msj; ?>
-									</div>
-								</div>
-							</div>	
-						
-						</div>
-						<!-- /.col-lg-6 (nested) -->
-						<div class="col-lg-6">
-						</div>
-						<!-- /.col-lg-6 (nested) -->
-					</div>
-					<!-- /.row (nested) -->
-				</div>
-				<!-- /.panel-body -->
-			</div>
-			<!-- /.panel -->
-		</div>
-		<!-- /.col-lg-12 -->
-	</div>
-	<!-- /.row -->
 </div>
-<!-- /#page-wrapper -->
