0b36214609c9b9e6694db52a64433056d61acb6e
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2018-04-15 11:35:51 -0600

    Se quita consulta que ya no se esta usando

--- application/modules/solicitud/models/Solicitud_model.php
+++ application/modules/solicitud/models/Solicitud_model.php
@@ -4,31 +4,6 @@
 
 		
 		/**
-		 * Lista de computadores
-		 * @author BMOTTAG
-		 * @since 23/3/2018
-		 */	
-		public function get_computadores($arrDatos)
-		{			
-			if (array_key_exists("idComputador", $arrDatos)) {
-				$this->db->where('id_computador', $arrDatos["idComputador"]);
-			}
-			
-			if (array_key_exists("estado", $arrDatos)) {
-				$this->db->where('estado', $arrDatos["estado"]);
-			}
-			
-			$this->db->order_by("computador_nombre", "asc"); 
-			$query = $this->db->get('computadores');
-
-			if ($query->num_rows() > 0) {
-				return $query->result_array();
-			} else {
-				return false;
-			}
-		}
-		
-		/**
 		 * Add/Edit Solicitud
 		 * @since 1/4/2018
 		 */
