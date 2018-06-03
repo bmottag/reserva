10fb73af2a3984dfabdfbdfdbc51eddd5bf2c203
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2018-06-03 12:42:22 -0600

    Solicitud: Cambio del titulo

--- application/modules/solicitud/views/solicitudes_usuario.php
+++ application/modules/solicitud/views/solicitudes_usuario.php
@@ -5,7 +5,8 @@
 		<div class="col-md-12 col-sm-12 col-xs-12">
 			<div class="x_panel">
 				<div class="x_title">
-					<h2><i class='fa fa-list'></i> LISTADO DE RESERVAS </h2> 
+					<h2><i class='fa fa-list'></i> LISTADO DE RESERVAS,<small> organizadas de la más reciente a las más antigua.</small>
+					</h2> 
 					
 					<ul class="nav navbar-right panel_toolbox">
 						<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
@@ -17,10 +18,6 @@
 
 				<div class="x_content">
 				
-					<div class="alert alert-warning alert-dismissible fade in" role="alert">
-						<strong>Info:</strong> Listado de solicitudes, organizadas de la más reciente a las más antigua.
-					</div>
-					
 <?php
 $retornoExito = $this->session->flashdata('retornoExito');
 if ($retornoExito) {
