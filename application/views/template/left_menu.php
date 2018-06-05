821eb911d96783e0e6f8211a2e83a72e35c22ab7
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2018-06-05 09:56:30 -0600

    Se coloca imagen del logo

--- application/views/template/left_menu.php
+++ application/views/template/left_menu.php
@@ -1,7 +1,9 @@
 <div class="col-md-3 left_col">
 	<div class="left_col scroll-view">
 		<div class="navbar nav_title" style="border: 0;">
-			<a href="<?php echo base_url("solicitud/solicitudes_usuario/" . $this->session->userdata("id")); ?>" class="site_title"><i class="fa fa-google-wallet"></i> <span>ZONA 2</span></a>
+			<a href="<?php echo base_url("solicitud/solicitudes_usuario/" . $this->session->userdata("id")); ?>" class="site_title">
+				<img src="<?php echo base_url("images/logo.png"); ?>" class="img-rounded" width="87" height="32" /> <span>ZONA 2</span>
+			</a>
 		</div>
 
 		<div class="clearfix"></div>
